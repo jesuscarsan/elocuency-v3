02c96570ddfc134bd217abc0c0e5ce046c25582e
Jesús A. Carballo Santaclara <10049849+jesuscarsan@users.noreply.github.com>
2026-02-09 22:44:10 +0100

    Delete apps/elo-obsidian-core-plugin directory

--- apps/elo-obsidian-core-plugin/src/Infrastructure/Obsidian/Utils/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ViewMode';
-export * from './Messages';
-export * from './Frontmatter';
-export * from './LocationPathBuilder';
-export * from './Vault';
-export * from './Strings';
